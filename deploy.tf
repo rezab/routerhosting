af34f6f5805c12f2a513cd823809f043ec9313fb
Reza <reza.b2008@gmail.com>
2020-11-01 16:03:24 +0330

    Update deploy.tf

--- deploy.tf
+++ deploy.tf
@@ -16,19 +16,11 @@ variable "imageId" {}
 variable "flavorId" {}
 variable "networkId" {}
 
-resource "openstack_compute_instance_v2" "Test-Terraform" {
-  name      = var.instanceName
-  region    = var.region
-  flavor_id = var.flavorId
-
-  block_device {
-    uuid                  = var.imageId
-    source_type           = "image"
-    volume_size           = 1
-    boot_index            = 0
-    destination_type      = "volume"
-    delete_on_termination = false
-  }
+resource "openstack_compute_instance_v2" "basic" {
+  name            = "basic"
+  image_id        = var.imageId
+  flavor_id       = var.flavorId
+  security_groups = ["default"]
 
   network {
     uuid = var.networkId
